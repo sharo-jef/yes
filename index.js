0f5d9850ed648847806eac9a0f5203c8b6749fb4
= <=>
2021-04-18 02:26:29 +0900

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+void async function main(argv) {
+    setInterval(() => {
+        if (argv._) {
+            console.log(argv._[0]);
+        } else {
+            console.log('y');
+        }
+    }, 1);
+}(argv);
+
